7373c45d3a09f05f8b9862e30ff1416609764295
unman <manjee.official@gmail.com>
2020-12-07 13:44:21 +0900

    AppComponent에 Stroage Module 추가

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -3,7 +3,7 @@ package com.example.android.dagger.di
 import com.example.android.dagger.registration.RegistrationActivity
 import dagger.Component
 
-@Component
+@Component(modules = [StorageModule::class])
 interface AppComponent {
 
     // RegistrationActivity 가 주입을 요청하고 @Inject 종속성을 제공해야함
