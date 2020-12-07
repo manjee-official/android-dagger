6e37251405ba819645b1ddabdbf0dd7d04b248d7
unman <manjee.official@gmail.com>
2020-12-07 19:19:37 +0900

    App Component에 userManager 사용하도록 수정

--- app/src/main/java/com/example/android/dagger/main/MainActivity.kt
+++ app/src/main/java/com/example/android/dagger/main/MainActivity.kt
@@ -31,10 +31,6 @@ import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    // @Inject annotated fields will be provided by Dagger
-    @Inject
-    lateinit var userManager: UserManager
-
     @Inject
     lateinit var mainViewModel: MainViewModel
 
@@ -45,11 +41,9 @@ class MainActivity : AppCompatActivity() {
      */
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        (application as MyApplication).appComponent.inject(this)
-
         super.onCreate(savedInstanceState)
 
-        userManager = (application as MyApplication).userManager
+        val userManager = (application as MyApplication).appComponent.userManager()
         if (!userManager.isUserLoggedIn()) {
             if (!userManager.isUserRegistered()) {
                 startActivity(Intent(this, RegistrationActivity::class.java))
@@ -61,7 +55,7 @@ class MainActivity : AppCompatActivity() {
         } else {
             setContentView(R.layout.activity_main)
 
-            mainViewModel = MainViewModel(userManager.userDataRepository!!)
+            userManager.userComponent!!.inject(this)
             setupViews()
         }
     }
