6822af7ac7441eb923e0c4f236ed90524799371f
unman <manjee.official@gmail.com>
2020-12-07 13:10:16 +0900

    AppComponent 추가
    - 빌드 에러 발생(Miss binding)

---
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -0,0 +1,11 @@
+package com.example.android.dagger.di
+
+import com.example.android.dagger.registration.RegistrationActivity
+import dagger.Component
+
+@Component
+interface AppComponent {
+
+    // RegistrationActivity 가 주입을 요청하고 @Inject 종속성을 제공해야함
+    fun inject(activity: RegistrationActivity)
+}
