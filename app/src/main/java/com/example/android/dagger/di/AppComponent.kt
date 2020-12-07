aa2b6f445641d0f0f4892c64e07777b0ed7af9dd
unman <manjee.official@gmail.com>
2020-12-07 15:32:22 +0900

    Dagger를 위한 리팩토링
    - 앱 죽는 현상 발생 (RegistrationViewModel의 인스턴스가 여러곳에 삽입됨)

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -3,6 +3,8 @@ package com.example.android.dagger.di
 import android.content.Context
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationActivity
+import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
+import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -20,5 +22,7 @@ interface AppComponent {
 
     // RegistrationActivity 가 주입을 요청하고 @Inject 종속성을 제공해야함
     fun inject(activity: RegistrationActivity)
+    fun inject(fragment: EnterDetailsFragment)
+    fun inject(fragment: TermsAndConditionsFragment)
     fun inject(activity: MainActivity)
 }
