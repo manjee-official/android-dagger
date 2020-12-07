e60fb892c3e0156765f02dab0927d7b39453a7c5
unman <manjee.official@gmail.com>
2020-12-07 15:39:29 +0900

    SubComponent 추가

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -2,15 +2,12 @@ package com.example.android.dagger.di
 
 import android.content.Context
 import com.example.android.dagger.main.MainActivity
-import com.example.android.dagger.registration.RegistrationActivity
-import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
-import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [StorageModule::class])
+@Component(modules = [StorageModule::class, AppSubComponents::class])
 interface AppComponent {
 
     // Factory to create instances of the AppComponent
@@ -20,9 +17,5 @@ interface AppComponent {
         fun create(@BindsInstance context: Context): AppComponent
     }
 
-    // RegistrationActivity 가 주입을 요청하고 @Inject 종속성을 제공해야함
-    fun inject(activity: RegistrationActivity)
-    fun inject(fragment: EnterDetailsFragment)
-    fun inject(fragment: TermsAndConditionsFragment)
     fun inject(activity: MainActivity)
 }
