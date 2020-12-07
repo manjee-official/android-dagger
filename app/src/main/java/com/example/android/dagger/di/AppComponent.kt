b7a63da0d0591996ed0ba530643541d3a1b6ad2c
unman <manjee.official@gmail.com>
2020-12-07 13:51:32 +0900

    @Component.Factory 추가

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -1,10 +1,19 @@
 package com.example.android.dagger.di
 
+import android.content.Context
 import com.example.android.dagger.registration.RegistrationActivity
+import dagger.BindsInstance
 import dagger.Component
 
 @Component(modules = [StorageModule::class])
 interface AppComponent {
+
+    // Factory to create instances of the AppComponent
+    @Component.Factory
+    interface Factory {
+        // With @BindsInstance, the Context passed in will be available in the graph
+        fun create(@BindsInstance context: Context): AppComponent
+    }
 
     // RegistrationActivity 가 주입을 요청하고 @Inject 종속성을 제공해야함
     fun inject(activity: RegistrationActivity)
